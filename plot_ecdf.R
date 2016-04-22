a253d967841659f9c594af0a8ac34dcc5d786505
andrewsr <andy.sommerlot@gmail.com>
2016-04-22 18:04:00 -0400

    first commit

---
+++ plot_ecdf.R
@@ -0,0 +1,37 @@
+plot_ecdf <- function(dat, legend_names = colnames(dat), title = 'ECDFs', xlab = 'value', ylab = 'probability'){
+  require(ggplot2)
+  require(reshape2)
+
+  ecdfs <- list()
+  for(i in 1:ncol(dat)){
+    ecdfs[[i]] = ecdf(dat[,i])
+  }
+
+  ecdf_frames <- list()
+  for(i in 1:length(ecdfs)){
+   now_dat <- data.frame(dat[,i], ecdfs[[i]](dat[,i]))
+    ecdf_frames[[i]] <- now_dat[order(now_dat[,1]),]
+  }
+
+  for(i in 1:length(ecdf_frames)){
+    colnames(ecdf_frames[[i]]) <- c('x', 'y')
+  }
+
+  colnames(cdfs) <- c('x', 'y')
+
+  melter <- data.frame(matrix(ncol = length(ecdf_frames) + 1, nrow = nrow(ecdf_frames[[1]])))
+  melter[,1] <- ecdf_frames[[1]][,2]
+  
+  for(i in 1:length(ecdf_frames)){
+    melter[,i+1] <- ecdf_frames[[i]][,1]
+  }
+
+  colnames(melter) <- c('X1', legend_names)
+
+  data_long <- melt(melter, id='X1')  
+
+  ggplot(data = data_long,
+    aes(x = value, y = X1, colour = variable)) +
+    labs(title = title, x = xlab, y = ylab) +
+    geom_line() 
+}
---
+++ test_plot_ecdf.R
@@ -0,0 +1,16 @@
+# test the function 
+#install.packages('ggplot2')
+#install.packages('reshape2')
+library(ggplot2)
+library(ecdf2)
+
+#get some example data
+a <- runif(100, 0, 100)
+b <-runif(100, 0, 100)
+c <-runif(100, 0, 100)
+d <- runif(100, 0, 100)
+e <- runif(100, 0, 100)
+
+dat <- data.frame(a,b,c,d,e)
+
+plot_ecdf(dat)
