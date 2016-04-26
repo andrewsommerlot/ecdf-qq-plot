0c0bf13c77f8d4cbcbc824c6b255492dc95fe3f4
andrewsommerlot <andy.sommerlot@gmail.com>
2016-04-26 16:01:57 -0400

    Cleaned up code and file name

--- plot_qq_compare.R
+++ plot_qq_compare.R
@@ -1,22 +1,22 @@
-plot_qq_compare = function(dat1, out_type = 'display', color_ramp = c('green', 'orange', 'red'), title = 'QQ Comparison', xlab = 'Base Variable', ylab = 'Comapre Variable', file_name = 'qq-test' ){
+plot_qq_compare <- function(dat1, out_type = 'display', color_ramp = c('green', 'orange', 'red'), title = 'QQ Comparison', xlab = 'Base Variable', ylab = 'Comapre Variable', file_name = 'qq-test' ){
   
   require(gridExtra)
   require(ggplot2)
   
-  q1 = list()
+  q1 <- list()
   for(i in 1:ncol(dat1)){
-    q1[[i]] = sort(dat1[,i])
+    q1[[i]] <-sort(dat1[,i])
   }
   
-  datin = data.frame(q1[[i]], q2[[i]])
-  test = seq(1, length(q2[[i]]), by = 1)
+  datin <- data.frame(q1[[i]], q2[[i]])
+  test <- seq(1, length(q2[[i]]), by = 1)
   
-  cost = abs(q2[[i]] - q1[[i]])
+  cost <- abs(q2[[i]] - q1[[i]])
   col_ramp <- colorRampPalette(color_ramp)(100)
   
   col_by_cost <- col_ramp[ceiling(norm_vec(cost)*99)+1]
   
-  plot_qq = ggplot(data = datin) +
+  plot_qq <- ggplot(data = datin) +
     aes(x = datin[,1], y = datin[,2]) +
     labs(title = title, x = xlab, y = ylab) +
     geom_point(size=4, alpha=1, color = col_by_cost) +
