20bd1007718cf0a650e3fe9d9945aa2825c8ec59
andrewsommerlot <andy.sommerlot@gmail.com>
2016-04-26 15:59:26 -0400

    Cleaned up = sign <- signs

--- plot_qq_compare_multi.R
+++ plot_qq_compare_multi.R
@@ -1,44 +1,44 @@
-plot_qq_compare_multi = function(dat_list, legend_names = colnames(dat_list[[1]])[2:length(colnames(dat_list[[1]]))], title = 'QQ Comparison', xlab = 'Base Variable' , ylab = 'Compare Variable', output_type = 'display', out_dir = getwd()){
+plot_qq_compare_multi <- function(dat_list, legend_names = colnames(dat_list[[1]])[2:length(colnames(dat_list[[1]]))], title = 'QQ Comparison', xlab = 'Base Variable' , ylab = 'Compare Variable', output_type = 'display', out_dir = getwd()){
   
   require(ggplot2)
   require(reshape2)
   require(Rmisc)
   
-  sorts = dat_list
-  melter = list()
-  dat_long = list()
+  sorts <- dat_list
+  melter <- list()
+  dat_long <- list()
   
   norm_vec <- function(v) (v - min(v))/diff(range(v))
   
   for(j in 1:length(dat_list)){
     for(i in 1:ncol(dat_list[[j]])){
-      sorts[[j]][,i] = sort(dat_list[[j]][,i])
+      sorts[[j]][,i] <- sort(dat_list[[j]][,i])
     }
   } 
   
-  melter = list()
-  lister = data.frame(matrix( ncol = length(dat_list), nrow = nrow(dat_list[[1]])))
+  melter <- list()
+  lister <- data.frame(matrix( ncol = length(dat_list), nrow = nrow(dat_list[[1]])))
   for(i in 1:ncol(dat_list[[1]])){
-    melter[[i]] = lister
+    melter[[i]] <- lister
   }
   
   
   for(i in 1:ncol(dat_list[[1]])){
     for(j in 1:length(dat_list)){
-      melter[[i]][,j] = data.frame(sorts[[j]][,i])
+      melter[[i]][,j] <- data.frame(sorts[[j]][,i])
     }
-    colnames(melter[[i]]) = c('observed', legend_names)
+    colnames(melter[[i]]) <- c('observed', legend_names)
   }
   
-  long = list()
+  long <- list()
   
   for(i in 1:length(melter)){
     long[[i]] <- melt(melter[[i]], id = 'observed')
   }
   
-  plots = list()
+  plots <- list()
   for(i in 1:length(long)){
-    plots[[i]] = ggplot(data = long[[i]]) +
+    plots[[i]] <- ggplot(data = long[[i]]) +
       aes(x = observed, y = value,  color = long[[i]]$variable) +
       labs(title = title, x = xlab, y = ylab, color = '') +
       geom_point(size=4, alpha=1) +
@@ -46,12 +46,12 @@ plot_qq_compare_multi = function(dat_list, legend_names = colnames(dat_list[[1]]
     
   }
   
-  plot_num = length(plots)
+  plot_num <- length(plots)
   if(length(plots) %% 2 == 1){
     plot_num = length(plots) + 1
   }
   
-  grid_cols = plot_num/2
+  grid_cols <- plot_num/2
   
   if(output_type == 'display'){
     multiplot(plotlist=plots,cols = grid_cols)
